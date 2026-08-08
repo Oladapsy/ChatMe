77fa18b71bd879a184628f9753e4fd73426ae97d
oladapsy <dapoodedeyi@yahoo.com>
2026-08-08 18:06:05 +0100

    chat

---
+++ src/features/chats/types/chat.ts
@@ -0,0 +1,12 @@
+export interface Chat {
+  id: string;
+  name: string;
+  avatar: string;
+  lastMessage: string;
+  time: string;
+  unreadCount?: number;
+  isPinned?: boolean;
+  isMuted?: boolean;
+  isArchived?: boolean;
+  isOnline?: boolean;
+}
