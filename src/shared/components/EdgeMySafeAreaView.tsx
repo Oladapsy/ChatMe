44d88bd172ed9f8b65a0a9b2da3ff00bde05cd49
oladapsy <dapoodedeyi@yahoo.com>
2026-08-29 20:33:20 +0100

    safe area view for chat screen

---
+++ src/shared/components/EdgeMySafeAreaView.tsx
@@ -0,0 +1,40 @@
+import React, { ReactNode } from "react";
+import { StyleSheet, ViewStyle, StyleProp, StatusBar, View } from "react-native";
+import {
+  SafeAreaView,
+  SafeAreaViewProps,
+} from "react-native-safe-area-context";
+
+interface MySafeAreaViewProps extends SafeAreaViewProps {
+  children: ReactNode;
+  style?: StyleProp<ViewStyle>;
+  color?: string;
+}
+
+const MySafeAreaView: React.FC<MySafeAreaViewProps> = ({
+  children,
+  style,
+  color,
+  edges = ["top", "left", "right"], // Exclude "bottom" by default so background reaches the edge
+  ...props
+}) => {
+  return (
+    <SafeAreaView
+      edges={edges}
+      style={[styles.container, style, color && { backgroundColor: color }]}
+      {...props}
+    >
+      <StatusBar barStyle="default" />
+      {children}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+});
+
+export default MySafeAreaView;
