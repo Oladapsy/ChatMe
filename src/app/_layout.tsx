8cef8714e262350aacbc3e0ab758033a8928d239
oladapsy <dapoodedeyi@yahoo.com>
2026-09-05 13:19:48 +0100

    Axios

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout() {
 
   const { isInitializing } = useInitializeAuth();
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+ 
   // creating tanstack query client
   const [queryClient] = useState(
     () =>
