01eaab975167418b8b14d5eaa20374d292ba0308
oladapsy <dapoodedeyi@yahoo.com>
2026-09-05 09:35:45 +0100

    save

--- src/features/auth/hooks/useVerifyOtp.ts
+++ src/features/auth/hooks/useVerifyOtp.ts
@@ -1,8 +1,22 @@
 import { useMutation } from "@tanstack/react-query";
 import { verifyOtp } from "@/features/auth/api/authApi";
+import { saveSession } from "@/services/authStorage";
 
 export function useVerifyOtp() {
   return useMutation({
     mutationFn: verifyOtp,
+
+    onSuccess: async (data) => {
+      const now = Date.now();
+
+      await saveSession({
+        accessToken: data.accessToken,
+        refreshToken: data.refreshToken,
+
+        accessTokenExpiresAt: now + data.accessTokenExpiresInSeconds * 1000,
+
+        refreshTokenExpiresAt: now + data.refreshTokenExpiresInSeconds * 1000,
+      });
+    },
   });
 }
