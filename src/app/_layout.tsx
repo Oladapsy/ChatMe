d2085abdae24f6004f719e436b86a3d67524eeda
oladapsy <dapoodedeyi@yahoo.com>
2026-09-07 23:23:43 +0100

    fix

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -66,12 +66,12 @@ export default function RootLayout() {
             <Stack.Screen name="index" />
             <Stack.Screen name="onboarding" />
             <Stack.Screen name="(auth)/verify-otp" />
-            <Stack.Screen name="(auth)/setup-profile" />
-            <Stack.Screen name="(auth)/upload-photo" />
-            <Stack.Screen name="(auth)/setup-pin" />
           </Stack.Protected>
           <Stack.Protected guard={isAuthenticated}>
+            <Stack.Screen name="(auth)/setup-profile" />
+            <Stack.Screen name="(auth)/upload-photo" />
             <Stack.Screen name="(tabs)" />
+            <Stack.Screen name="(auth)/setup-pin" />
           </Stack.Protected>
         </Stack>
       </GestureHandlerRootView>
