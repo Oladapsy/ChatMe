691bb59f762b5bef10410e367c45517edba21e03
oladapsy <dapoodedeyi@yahoo.com>
2026-09-08 09:53:38 +0100

    added

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -2,13 +2,20 @@ import { create } from "zustand";
 
 type AuthState = {
   isAuthenticated: boolean;
+  profileComplete: boolean;
+
   setAuthenticated: (value: boolean) => void;
+  setProfileComplete: (value: boolean) => void;
 };
 
 export const useAuthStore = create<AuthState>((set) => ({
   isAuthenticated: false,
+  profileComplete: false,
 
   setAuthenticated: (value) => {
     set({ isAuthenticated: value });
+  },
+  setProfileComplete: (value) => {
+    set({ profileComplete: value });
   },
 }));
